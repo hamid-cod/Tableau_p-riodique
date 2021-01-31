b488280cafbb5c88d3830719bf8498c7035a38cd
hamid-cod <hamanhamid4@gmail.com>
2021-01-31 15:41:02 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
-	function htpRequest(file, selector) {
+	function tableau_periodique(file, selector) {
 		var url = file;
 		var xmlhttp = new XMLHttpRequest();
 		xmlhttp.onreadystatechange = function() {
@@ -10,35 +10,107 @@ $(document).ready(function(){
 				var rows = data['rows'];
 				var cols = data['cols'];
 				var result;
-				var x = '<table>';
+				var x = '';
+				x += '<table>';
+				x += '<thead>';
 				x += '<tr>';
 				for (var i = 0; i <= cols; i++){
-					if (i==0) {x += '<td class="vide group"></td>';}
-					else {x += '<td class="group">'+ i +'</td>';}
+					if (i==0) {x += '<th class="vide group period"></th>';}
+					else {x += '<th class="group col'+i+'">'+ i +'</th>';}
 				}
 				x += '</tr>';
+				x += '</thead>';
+				x += '<tbody>';
 				for (var r = 1; r <= rows; r++) {
 					x += '<tr>';
-					x += '<td class="period">'+r+'</td>';
+					if (r <= 7) {x += '<td class="period row'+r+'">'+r+'</td>';}
+					else if (r == 8) {
+						x += '<td class="vide period"></td>';
+					}
+					else if (r == 9) {
+						x += '<td class="vide period"></td><td colspan="2" class="Lanthanide">'+ele[56].category+'</td>';
+					}
+					else if (r == 10) {
+						x += '<td class="vide period"></td><td colspan="2" class="Actinide">'+ele[88].category+'</td>';
+					}
+					// else {x += '<td class="vide"></td>';}
 					for (var c = 1; c <= cols; c++) {
 						result = ele.filter(ele => ele.ypos == r && ele.xpos == c);
-						if (result[0] === undefined) { x += '<td class="vide"></td>'; }
+						if (result[0] === undefined) { 
+							if (r == 9 || r == 10) {}
+							else {x += '<td class="vide"></td>';}
+						}
 						else{ 
-							x += '<td>';
-							x += '<div class="z"><span>'+result[0].number+'</span><span class="z">'+result[0].phase+'</span></div>';
-							x += '<div class="symbol"><span>'+result[0].symbol+'</span></div>';
-							x += '<div class="name"><span>'+result[0].name+'</span></div>';
+							x += '<td class="element row'+((r>7)?(r-3):r)+' col'+((r>7)?3:c)+' '+ ((r>8&&r==9)?'lanthanide':'')+((r>8&&r==10)?'actinide':'') +'">';
+								x += '<div class="z">';
+									x += '<span class="number">'+result[0].number+'</span>';
+									x += '<span class="phase">'+result[0].phase+'</span>';
+								x += '</div>';
+								x += '<div class="symbol"><span>'+result[0].symbol+'</span></div>';
+								x += '<div class="name"><span>'+result[0].nom+'</span></div>';
 							x += '</td>';
 						}
 					}
 					x += '</tr>';
 				}
+				x += '</tbody>';
 				x += '</table>';
 				$(selector).append(x);
+				$('.group').hover(
+					function() {
+						$('.col'+$(this).html()).css('background-color','rgba(255,0,0,0.5)');
+					},
+					function() {
+						$('.col'+$(this).html()).removeAttr('style');
+					}
+				);
+				$('.period').hover(
+					function() {
+						$('.row'+$(this).html()).css('background-color','rgba(255, 242, 0, 0.7)');
+					},
+					function() {
+						$('.row'+$(this).html()).removeAttr('style');
+					}
+				);
+				$('.Lanthanide').hover(
+					function() {
+						$('.Lanthanide,.lanthanide').css('background-color','orange');
+					},
+					function() {
+						$('.Lanthanide,.lanthanide').removeAttr('style');
+					}
+				);
+				$('.Actinide').hover(
+					function() {
+						$('.Actinide,.actinide').css('background-color','#fdcb6e');
+					},
+					function() {
+						$('.Actinide,.actinide').removeAttr('style');
+					}
+				);
+				$('.element').click(function() {
+					var n = $(this).find(".number").html() - 1;
+					Swal.fire({
+					  title: '<strong><u>'+ele[n].nom+'</u></strong>',
+					  html:
+					  	'<table class="table table-striped">'+
+						    '<tr><td>Nombre atomique </td><td><b>'+ele[n].number+'</b></td></tr>' +
+						    '<tr><td>Symbole </td><td><b>'+ele[n].symbol+'</b></td></tr>' +
+						    '<tr><td>Configuration electronique </td><td><b>'+ele[n].electron_configuration_semantic+'</b></td></tr>' +
+						    '<tr><td>Etat physique </td><td><b>'+((ele[n].phase != 'Gas')?ele[n].phase+'e':ele[n].phase)+'</b></td></tr>'+
+					    '</table>',
+					  showCloseButton: true,
+  					showCancelButton: true,
+					  confirmButtonText:
+					    '<a href="'+ele[n].source+'" target="_blank"> Plus d\'info</a>',
+					  cancelButtonText: 'No'
+					});
+				});
 		  }
 		};
 		xmlhttp.open("GET", url, true);
 		xmlhttp.send(null);
 	}
-	htpRequest("data/tp.json", "#show");
+	tableau_periodique("data/tp.json", "#show");
+	
 });
